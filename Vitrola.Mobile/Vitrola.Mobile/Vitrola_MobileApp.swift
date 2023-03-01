7b75cee9e513a6a9145a6139e312cf1a46a75b70
Natanael <NatanaelUF17@gmail.com>
2023-02-28 20:56:23 -0400

    Uploading all proyects

---
+++ Vitrola.Mobile/Vitrola.Mobile/Vitrola_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vitrola_MobileApp.swift
+//  Vitrola.Mobile
+//
+//  Created by Natanael Ureña Fernández  on 28/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Vitrola_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
